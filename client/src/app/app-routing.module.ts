b40a7bca61f631838b9cf2c1a6813776b8895f57
unknown <drmorzek@mail.ru>
2021-08-15 19:50:48 +0300

    add authlayout

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+
+const routes: Routes = [
+  {
+    path: "", component: AuthLayoutComponent,
+    children: [
+      { path: "", redirectTo: "/login", pathMatch: "full"},
+      { path: "login", component: LoginPageComponent},
+      { path: "register", component: RegisterPageComponent}
+    ]
+  },
+  {
+    path: "", component: SiteLayoutComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
